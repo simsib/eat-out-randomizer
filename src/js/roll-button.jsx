3397d7fa680c6fe1b6da5f0b2740d47ce93bc479
weedma <vydmantas.kolduba@gmail.com>
2017-02-13 22:06:09 +0200

    added roll button

---
+++ src/js/roll-button.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class RollButton extends React.Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default RollButton;
