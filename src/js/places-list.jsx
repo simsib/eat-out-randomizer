1e234789810607645fa51ef0518f11a3fb8b5a2d
weedma <vydmantas.kolduba@gmail.com>
2017-02-14 23:40:31 +0200

    formating

--- src/js/places-list.jsx
+++ src/js/places-list.jsx
@@ -4,15 +4,15 @@ class PlacesList extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            active: Math.floor(Math.random()*props.places.length)
+            active: Math.floor(Math.random() * props.places.length)
         }
         console.log(this.state.active);
     }
     renderPlaces() {
         return this.props.places.map(
-            (place,i) =>{
-                let isActive = this.state.active === i? 'active':''; 
-                return <a href="#!" key={i} className={"collection-item "+ isActive}>{place}</a>
+            (place, i) => {
+                let isActive = this.state.active === i ? 'active' : '';
+                return <a href="#!" key={i} className={"collection-item " + isActive}>{place}</a>
             }
         );
     }
