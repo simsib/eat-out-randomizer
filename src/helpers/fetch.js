7fd656523c28b888c53b9700b41c2581d7c04e0b
weedma <vydmantas.kolduba@gmail.com>
2017-02-17 15:38:20 +0200

    create helper class for retrieving data

---
+++ src/helpers/fetch.js
@@ -0,0 +1,27 @@
+
+class FetchApi {
+    constructor(props) {
+        this.path = 'https://raw.githubusercontent.com/simsib/eat-out-randomizer/master/data/data.json';
+    }
+
+    getData(callback) {
+        //setTimeout(function () {
+            fetch('https://raw.githubusercontent.com/simsib/eat-out-randomizer/master/data/data.json')
+                .then((response) => {
+                    if (response.status !== 200) {
+                        console.log(`Looks like there was a problem. Status Code: ${response.status}`);
+                        return;
+                    }
+                    response.json().then((data) => {
+                        callback(data.places);
+                    });
+                })
+                .catch((err) => {
+                    console.log('Fetch Error :-S', err);
+                });
+        //}, 1000);
+    }
+}
+
+
+export default FetchApi;
