72dc920f18dade3c47c1e52fe8244c16f8c1d136
weedma <vydmantas.kolduba@gmail.com>
2017-02-22 22:26:11 +0200

    show loader component when data is not loaded

--- src/js/main-section.jsx
+++ src/js/main-section.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import RollButton from './roll-button.jsx';
 import PlacesList from './places-list.jsx';
+import Loader from './loader.jsx';
 import FetchApi from '../helpers/fetch';
 
 export default class MainSection extends React.Component {
@@ -12,24 +13,40 @@ export default class MainSection extends React.Component {
             active: -1 // because nothing was received yet
         }
     }
-    updatePlaces(data){
-        this.setState({ places: data,active:this.randomNumber(data) })
+    updatePlaces(data) {
+        this.setState({ places: data, active: this.randomNumber(data) })
     }
     componentWillMount() {
-        (new FetchApi()).getData(this.updatePlaces.bind(this));
+        (new FetchApi())
+            .getData()
+            .then((data) => {
+                this.updatePlaces(data);
+            })
+            .catch((e) => console.log(e));
     }
+
     render() {
-        return (
-            <div className="section no-pad-bot" id="index-banner">
-                <div className="container">
-                    <br /><br />
-                    <h1 className="header center orange-text">Whoop Whoop!</h1>
-                    <PlacesList places={this.state.places} activeItem={this.state.active}></PlacesList>
-                    <RollButton RollHandler={this.RollHandler.bind(this)}></RollButton>
-                    <br /><br />
+            return (
+                <div className="section no-pad-bot" id="index-banner">
+                    <div className="container">
+                        <h1 className="header center orange-text">{this.props.header}</h1>
+                        {this.renderContent()}
+                    </div>
                 </div>
-            </div>
-        )
+            )
+    }
+    renderControls() {
+        return (
+            <div>
+                <PlacesList places={this.state.places} activeItem={this.state.active}></PlacesList>
+                <RollButton RollHandler={this.RollHandler.bind(this)}></RollButton>
+                <br /> <br />
+            </div>)
+    }
+    renderContent(){
+        return this.state.places.length > 0 ?
+            this.renderControls():
+            <Loader></Loader>     
     }
     RollHandler() {
         this.setState({ active: this.randomNumber(this.state.places) });
